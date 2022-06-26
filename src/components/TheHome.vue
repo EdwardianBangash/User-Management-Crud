982cc76e892a036c4503a5760374ca0b63170092
Kashif Ahmad <ahmadkashif773@gmail.com>
2022-06-26 12:47:28 +0500

    structure

---
+++ src/components/TheHome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex">
+        <div class="flex-auto w-75 ...">
+    02
+  </div>
+  <div class="flex-auto w-25 ...">
+    03
+  </div>
+            <!-- Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga cum provident autem laudantium eos incidunt facere. Neque blanditiis reprehenderit modi numquam asperiores rerum quisquam. Dolore pariatur a optio voluptatum at cumque alias sapiente dolorum! A pariatur tempore, praesentium tempora, necessitatibus, expedita itaque dolore repudiandae consequuntur sequi reiciendis nisi omnis? Quae.
+            <button class="float-right">hello world</button> -->
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
